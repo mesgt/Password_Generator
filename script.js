6790449323079f04d1aa112edfbb2cae8c72497c
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-03 00:07:25 -0600

    Organized code in a more logical way.

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ function writePassword() {
   passwordText.value = password; 
 }
 
+function generatePassword() {
+  var charTotal = "";
+    if (charLower===true) {
+      charTotal += passParameter.LowerCase;}
+    if (charUpper===true) {
+      charTotal += passParameter.UpperCase;}
+    if (charNumber===true) {
+      charTotal +=passParameter.Numbers;}
+    if (charSpecial===true) {
+      charTotal +=passParameter.Special;}
+      
+  console.log(charTotal); //take out when done.
+  
+  var finalPassword="";
+  for (let i = 0; i < passLength; i++) {
+    finalPassword+=charTotal[Math.floor(Math.random()*passLength+15)];  //WHY DO I NEED + NUMBER? AND WHY DOES THE PASSWORD CONTAIN ONLY LOWER CASE LETTERS IF THAT NUMBER IS 0?
+  }
+  return finalPassword;
+}
+
 var passParameter = {
   LowerCase : "qwertyuiopasdfghjklzxcvbnm",
   UpperCase : "QWERTYUIOPASDFGHJKLZXCVBNM",
@@ -21,11 +41,9 @@ var passParameter = {
 function getPassLength() {
   var pLength = prompt("Select the length of your password by typing in a number between 8 and 128.");
     if ((pLength < 8) || (pLength > 128)) {
-      alert("Please select appropriate number of characters for your password.");
-    }
+      alert("Please select appropriate number of characters for your password.");}
     else {
-      alert("You have selected your password to be " + pLength + " characters long.");
-    }
+      alert("You have selected your password to be " + pLength + " characters long.");}
     return pLength
 }
 passLength=getPassLength()
@@ -35,30 +53,11 @@ var charUpper = confirm("Would like an upper case letter in your password?");
 var charNumber = confirm("Would like a number in your password?");
 var charSpecial = confirm("Would like a special character in your password?");
 
+writePassword();
+
 //checks. Delete from final product.
 console.log(passLength);
 console.log(charLower);
 console.log(charUpper);
 console.log(charNumber);
 console.log(charSpecial);
-
-function generatePassword() {
-  var charTotal = "";
-    if (charLower===true) {
-      charTotal += passParameter.LowerCase;}
-    if (charUpper===true) {
-      charTotal += passParameter.UpperCase;}
-    if (charNumber===true) {
-      charTotal +=passParameter.Numbers;}
-    if (charSpecial===true) {
-      charTotal +=passParameter.Special;}
-
-  console.log(charTotal); //take out when done.
-  var finalPassword="";
-  for (let i = 0; i < passLength; i++) {
-    finalPassword+=charTotal[Math.floor(Math.random()*passLength+15)];  //WHY DO I NEED + NUMBER? AND WHY DOES THE PASSWORD CONTAIN ONLY LOWER CASE LETTERS IF THAT NUMBER IS 0?
-  }
-  return finalPassword;
-}
-
-writePassword();
